c006a45d81c175d34e84c21d5a0de9a5215fbe0e
Ilias <cepctapk@gmail.com>
2023-01-28 19:50:21 +0300

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Commander on 28.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
